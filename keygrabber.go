ff11f41b324ace93df0161ace29ddc6c031ac399
Josh Gordon <github@joshgordon.net>
2016-10-05 21:22:49 -0400

    Config parser working

--- keygrabber.go
+++ keygrabber.go
@@ -5,31 +5,53 @@ import (
 	"github.com/BurntSushi/toml"
 	"log"
 	"os"
+  "http"
 )
 
 type Config struct {
-	baseurl    string
-	keyfile    string
-	sigfile    string
-	trustedSig string
+	Baseurl    string
+	Keyfile    string
+	Sigfile    string
+	TrustedSig string
 }
+
+
+//Parse the config file and return a config struct.
+func ParseConfig(configFile string) (Config, error) {
+  _, err := os.Stat(configFile)
+  if err != nil {
+    return Config{}, err
+  } else {
+    var config Config
+    if _, err := toml.DecodeFile(configFile, &config); err != nil {
+      return Config{}, err
+    } else {
+      return config, err
+    }
+  }
+}
+
+func GetFileFromURL(url string) (err) {
+  resp, err := http.Get(url)
+  defer resp.close()
+
+  tempFile, err := ioutil.TempFile("", "keygrabber")
+  defer tempFile.close()
+
+  
+}
+
 
 func main() {
 	if len(os.Args) >= 2 {
 		fmt.Println("HI. I'm alive")
 
-		var configFile = os.Args[1]
-		_, err := os.Stat(configFile)
-		if err != nil {
-			log.Fatal("Config file is missing", configFile)
-		} else {
-
-			var config Config
-			if _, err := toml.DecodeFile(configFile, &config); err != nil {
-				log.Fatal(err)
-			}
-			fmt.Printf("%+v\n", config)
-		}
+    config, err := ParseConfig(os.Args[1])
+    if err == nil {
+      fmt.Printf("%+v\n", config)
+    } else {
+      log.Fatal(err)
+    }
 	} else {
 		log.Fatal("must specify configuration file")
 	}
